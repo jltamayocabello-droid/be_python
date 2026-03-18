88fa28db7b917e2acfbf078accb93ed1de62339e
Jorge <jltamayocabello@gmail.com>
2026-03-18 17:49:44 -0300

    Variables

---
+++ ej_03_variables.py
@@ -0,0 +1,14 @@
+# dinamico
+dinamico = 10
+dinamico = "elemento"
+dinamico = 290.333
+print(dinamico)
+
+# type
+print(type(dinamico))
+
+# constante
+PI = 3.1416 # Mayúscula para señalar que no se cambie
+print(PI)
+
+
