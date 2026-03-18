dddb9a68356c8768a15bf66166992f949d3f2f88
Jorge <jltamayocabello@gmail.com>
2026-03-18 18:06:29 -0300

    Entradas y salidas

---
+++ ej_04_entrada-salida.py
@@ -0,0 +1,12 @@
+# input / Entrada
+nombre = input("Ingrese su nombre: ")
+print("Su nombre es: " + nombre)
+
+# Conversión de tipos / casting
+edad = input("Ingrese su edad: ")
+edad = int(edad) # Se transforma el string en entero
+print(edad)
+
+# input / salida
+print(f"Su nombre es: {nombre} y su edad es: {edad}") # formateo de salida
+
