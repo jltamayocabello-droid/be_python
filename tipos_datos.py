6b2935870d4f2993d333b4ccedca000645ee6c26
Jorge <jltamayocabello@gmail.com>
2026-03-18 17:16:16 -0300

    Tipos de datos

---
+++ tipos_datos.py
@@ -0,0 +1,30 @@
+x = 10 # int
+y = 3.14 # float 
+z = "Palabra" # str
+activo = True # bool
+
+# int / Enteros
+a = 101
+b = 33
+print(a + b)
+print(a - b)
+print(a * b)
+
+# float / Decimales
+a = 10.5
+b = 3.14
+print(a + b)
+print(a - b)
+print(a * b)
+
+# str / String
+a = "Bienvenidos"
+b = " gente"
+print(a + b)
+
+# bool / Booleanos
+a = True
+b = False
+print(a and b)
+print(a or b)
+print(not a)
