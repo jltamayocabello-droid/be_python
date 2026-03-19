471ae23cb136a1823e09748c54978e4ac97a91b0
Jorge <jltamayocabello@gmail.com>
2026-03-19 11:06:24 -0300

    Condicionales

---
+++ ej_06_condicionales.py
@@ -0,0 +1,20 @@
+# if
+edad = 23
+if edad >= 18:
+    print("Es mayor de edad")
+
+# elif
+nota = 5.5
+if nota >= 6.0:
+    print("Excelente nota")
+elif nota >= 4.0:
+    print("Aprobado")
+
+# else
+nota = 2.5
+if nota >= 6.0:
+    print("Excelente nota")
+elif nota >= 4.0:
+    print("Aprobado")
+else:
+    print("Reprobado")
