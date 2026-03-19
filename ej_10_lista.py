781031cf688e7bd170b48eaf00b979e570d2f265
Jorge <jltamayocabello@gmail.com>
2026-03-19 11:38:05 -0300

    Listas

---
+++ ej_10_lista.py
@@ -0,0 +1,22 @@
+numeros = [10, 20, 30, 40]
+print(numeros)
+
+# Indice
+print(numeros[0])
+
+# Agregar al final
+numeros.append(50)
+print(numeros)
+
+# Agregar con insert
+numeros.insert(0, 0)
+numeros.insert(3, 25)
+print(numeros)
+
+# Remover
+numeros.remove(0)
+print(numeros)
+
+# Borrar el ultimo
+numeros.pop()
+print(numeros)
