e1f8d96b068987b5f2fa83b46c43b23435f0617f
Jorge <jltamayocabello@gmail.com>
2026-03-19 11:58:32 -0300

    Diccionarios

---
+++ ej_13_diccionarios.py
@@ -0,0 +1,15 @@
+persona = {
+    "nombre": "Pedro",
+    "edad": 30,
+    "ciudad": "Sao Paulo"
+}
+
+print(persona)
+print(persona["nombre"])
+print(persona["edad"])
+print(persona["ciudad"])
+
+# List Comprehension
+cuadrado = [x ** 2 for x in range(1, 6)]
+print(cuadrado)
+
