2d0d8fb6d65f6cb51726ffd801031cfd3b58ab47
Jorge <jltamayocabello@gmail.com>
2026-03-19 11:49:22 -0300

    Conjuntos

---
+++ ej_12_conjuntos.py
@@ -0,0 +1,13 @@
+numero = {1, 2, 3, 4}
+print(numero)
+
+numero2 = {5, 6, 7, 8}
+print(numero2)
+
+# Unir dos conjuntos
+union = numero | numero2
+print(union)
+
+# Diferencia
+diferencia = numero - numero2
+print(diferencia)
