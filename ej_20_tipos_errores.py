bede3e525eeb0bf475cd07a87a261f1e9368713d
Jorge <jltamayocabello@gmail.com>
2026-03-30 21:28:47 -0300

    Tipos de errores

---
+++ ej_20_tipos_errores.py
@@ -0,0 +1,12 @@
+# Errores sintasis
+# print("hola"
+
+# Errores de ejecución
+resultado = 10 / 0
+print(resultado)
+      
+# Errores de logica
+def calcular_area_rectangulo(base, altura):
+    return base + altura
+
+calcular_area_rectangulo(19.2, 23,6)
