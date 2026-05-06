cc04e51142d911c04bfa5e13049dda46a56d870c
Jorge <jltamayocabello@gmail.com>
2026-05-06 18:14:34 -0400

    Programación orientada a objetos

---
+++ ej_23_poo.py
@@ -0,0 +1,14 @@
+# Clases
+
+class Persona:
+    def __init__(self, nombre, edad):
+        self.nombre = nombre
+        self.edad = edad
+
+# Objeto
+
+persona1 = Persona("Juan", 23)
+print(persona1.nombre)
+print(persona1.edad)
+
+# Atributos
