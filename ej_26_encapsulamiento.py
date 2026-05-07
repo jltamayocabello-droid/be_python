30f85fe3cc89d963b35046f035bbca3cc75b2b44
Jorge <jltamayocabello@gmail.com>
2026-05-06 23:50:42 -0400

    Encapsulamiento

---
+++ ej_26_encapsulamiento.py
@@ -0,0 +1,24 @@
+class CuentaBancaria:
+    def __init__(self, titular, saldo):
+        self.titular = titular
+        self.__saldo = saldo  # Atributo privado
+
+    def obtener_saldo(self):
+        return self.__saldo  # Acceso al atributo privado
+    
+    def depositar(self, cantidad):
+        if cantidad > 0:
+            self.__saldo += cantidad
+            return f"Deposito exitoso, nuevo saldo: {self.__saldo}"
+        else:
+            return "Cantidad invalida"
+        
+    def retirar(self, cantidad):
+        if 0 < cantidad <= self.__saldo:
+            self.__saldo -= cantidad
+            return f"Retiro exitoso, nuevo saldo: {self.__saldo}"
+        else:
+            return "Saldo insuficiente o cantidad invalida"
+        
+cuenta1 = CuentaBancaria("Alberto Rozas", 2000)
+print(cuenta1.obtener_saldo)
