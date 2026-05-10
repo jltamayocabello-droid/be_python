a1b17b79a362df5dd6507cc14c42d942bdeef7d7
Jorge <jltamayocabello@gmail.com>
2026-05-09 23:02:10 -0400

    Se recupera datos.pkl

--- ej_35_pickle.py
+++ ej_35_pickle.py
@@ -7,3 +7,7 @@ datos = {
 
 with open("datos.pkl", "wb") as archivo:
     pickle.dump(datos, archivo)
+
+with open("datos.pkl", "rb") as archivo:
+    datos_cargados = pickle.load(archivo)
+print(datos_cargados)    
